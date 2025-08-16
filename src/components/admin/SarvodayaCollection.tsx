3b2711f8622e3e381f9bf4b9fdf5d9b216c50e47
Vipin-koroth <kvipin00@gmail.com>
2025-08-16 10:54:28 +0530

    Updated SarvodayaCollection.tsx

--- src/components/admin/SarvodayaCollection.tsx
+++ src/components/admin/SarvodayaCollection.tsx
@@ -795,23 +795,6 @@ const SarvodayaCollection: React.FC = () => {
           </>
         )}
       </div>
-    </div>
-  );
-};
-
-export default SarvodayaCollection;
-          <button
-            onClick={() => setActiveView('overview')}
-            className={`px-4 py-2 rounded-lg font-medium transition-colors ${
-              activeView === 'overview'
-                ? 'bg-blue-600 text-white'
-                : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
-            }`}
-          >
-            System Overview
-          </button>
-          
-      </div>
 
       {/* Add/Edit Collection Entry Modal */}
       {showAddModal && (
